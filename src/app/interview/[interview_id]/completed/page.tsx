62fc2260a2a62d9ffd450b805dcc3dc6a8fdaa34
Beqael <beqaelashvili3@gmail.com>
2025-06-24 14:24:45 +0400

    add_interview_completed_page

--- src/app/interview/[interview_id]/completed/page.tsx
+++ src/app/interview/[interview_id]/completed/page.tsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { Check, Send } from "lucide-react";
+import Image from "next/image";
 
 function InterviewComplete() {
-  return <div>InterviewComplete</div>;
+  return (
+    <div className="flex justify-center p-8  md:p-20 ">
+      <div className="flex flex-col text-center items-center gap-4">
+        <div className="bg-green-500 rounded-full p-4">
+          <Check className="h-12 w-12" />
+        </div>
+        <h1 className="text-3xl font-bold">Interview Complete!</h1>
+        <p className="text-gray-500">
+          Thank you participating in the AI-driven interview with AIcruiter
+        </p>
+        <Image
+          src={"/interview_ilustration.jpg"}
+          alt="image"
+          width={500}
+          height={500}
+          className="w-[800px] rounded-xl"
+        />
+        <div className="bg-white w-full flex flex-col p-4 items-center justify-center rounded-xl">
+          <div className="bg-blue-500 rounded-full p-4">
+            <Send className="h-6 text-white w-6" />
+          </div>
+          <h2 className="text-xl font-bold">What's Next?</h2>
+          <p className="text-sm text-gray-500">
+            The recruiter review your interview responses and will contact you
+            soon regarding the next steps.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default InterviewComplete;
